236762af603f5e501ebf219a57b02b84b9258774
MarciaSolano <99497342+MarciaSolano@users.noreply.github.com>
2022-07-28 13:56:06 -0500

    Actividad Didáctica

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,24 +7,6 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/sopa-de-letras.png", alt="Texto que describa la imagen")
-      .col-12.col-md-8.col-lg-9
-        .titulo-segundo
-          h2 Sopa de letras 
-        p.mb-4 #[b Objetivo] Encontrar los términos descritos en la sopa de letras para afianzar los conceptos relacionados en el presente contenido. El aprendiz relaciona los conceptos y previene el deterioro del desarrollo cognitivo. 
-        .tarjeta.actividad.p-3
-          .row.justify-content-around.align-items-center            
-            .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Sopa de letras 
-            .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
-                span Realizar
-                i.fas.fa-puzzle-piece
-    
-    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
-      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
-        .tarjeta.h-100.d-flex.align-items-center.p-4
-          figure
             img(src="@/assets/template/arrastrar-soltar.png", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
@@ -35,7 +17,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Arrastrar y soltar en el lugar correcto 
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/CF1AD/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
